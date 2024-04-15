3433b3e67ebebb14d767847ba48c33333dfb2918
nehakumari17 <nehak772903@gmail.com>
2024-04-15 20:25:50 +0530

    added frontend api for backend

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express()
 const PORT = 3000
 
 const corsOptions = {
-    origin: ["http://localhost:5173"],
+    origin: "https://email-otp-verification-frontend-f4lg.onrender.com",
     method: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
     credentials: true
 }

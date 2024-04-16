a9bb18c8c6419099d5fcb3e77506e7bde048c94f
nehakumari17 <nehak772903@gmail.com>
2024-04-16 12:21:25 +0530

    added frontend api to backend

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "https://email-otp-verification-backend-i4n6.onrender.com/register",
+        "https://email-otp-verification-backend-i4n6.onrender.com/api/register",
         formData
       );
       if (response.status === 200) {

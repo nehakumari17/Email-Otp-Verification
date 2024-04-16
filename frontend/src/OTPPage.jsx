7a28528149c27f79e23f192f4276c7b484e41ef2
nehakumari17 <nehak772903@gmail.com>
2024-04-16 12:27:14 +0530

    added frontend api to backend

--- frontend/src/OTPPage.jsx
+++ frontend/src/OTPPage.jsx
@@ -26,7 +26,7 @@ const OTPPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://email-otp-verification-backend-i4n6.onrender.com/api/verify-otp', { userId: userId, otp: otp });
+      const response = await axios.post('http://localhost:3000/api/verify-otp', { userId: userId, otp: otp });
       setVerificationResult(response.data);
       if (response.status === 200) {
         navigate('/login');

cb32188399decdb47654598fae6886bcbbfa627a
nehakumari17 <nehak772903@gmail.com>
2024-04-16 06:21:09 +0530

    added frontend api to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,29 @@
-import Login from "./Login"
-import OTPPage from "./OTPPage"
-import Register from "./Register"
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Login from "./Login";
+import OTPPage from "./OTPPage";
+import Register from "./Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useDispatch } from 'react-redux';
 import { useEffect } from "react";
 
 function App() {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
+    // Do something with the token if needed
   }, [dispatch]);
 
   return (
-    <div>
-      <Routes>
-      <Route path="/" element={<Register />}/>
-      <Route path="/login" element={<Login />}/>
-      <Route path="/otp" element={<OTPPage />}/>
-      </Routes>
-    </div>
-  )
+    <Router> {/* Wrap your routes inside the Router component */}
+      <div>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/otp" element={<OTPPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
